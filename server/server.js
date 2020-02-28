f17f57f06c774ba12be3148d56071a16cb786e7b
ferdinando-83 <58439826+ferdinando-83@users.noreply.github.com>
2020-02-28 17:38:21 -0500

    Update server.js
    Add bodyParser and MongoDB implementation.

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@
 import express from 'express';
 import router from './routes/routes.js'
 import path from 'path';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb', extended: false}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended:false}));
+
+mongosse.connect('mongodb info goes here');
 
 app.use('/', router);
 
