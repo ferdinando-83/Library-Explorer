081044856500b767c53285236257435260e9283f
ferdinando-83 <58439826+ferdinando-83@users.noreply.github.com>
2020-02-28 14:00:22 -0500

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,7 @@
 //server/routes/routes.js
 import express from 'express';
 
-let router = express.Router();
+const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index')
