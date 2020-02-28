b9286bfdf5a11c102916903d771a4d68eda09a26
ferdinando-83 <58439826+ferdinando-83@users.noreply.github.com>
2020-02-28 12:13:51 -0500

    Update App.js
    Add boilerplate, title, and "Welcome" message.

--- client/components/App.js
+++ client/components/App.js
@@ -1,1 +1,20 @@
+//client/components/App.js
+import React, { Component } from 'react';
+import '../css/App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Begin Your Journey</h1>
+        </header>
+        <p className="App-intro">
+          Welcome
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
