0e07044d910eb28aa7c1e79286a9121843403c8f
ferdinando-83 <58439826+ferdinando-83@users.noreply.github.com>
2020-02-28 15:32:16 -0500

    Create webpack.config.js
    Add webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+//webpack.config.js
+import path from 'path';
+import webpack from 'webpack';
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'client'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react']
+      }
+    },
+    {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }]
+  }
+}
