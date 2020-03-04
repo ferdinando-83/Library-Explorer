3f761c50473679868bfa32a5de0ee42abb5f90af
ferdinando-83 <58439826+ferdinando-83@users.noreply.github.com>
2020-03-04 12:21:10 -0500

    Update Add.js
    Update Create implementation for CRUD functionality.

--- client/components/Add.js
+++ client/components/Add.js
@@ -110,5 +110,56 @@ class Add extends React.Component {
           </Button>
           <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} contentLabel="Add Destination" className="Modal">
           <Link to={{pathname: '/', search: ''}} style={{ textDecoration: 'none' }}>
-          
-          //finish later
+            <Button bsStyle="danger" bsSize="mini" onClick="{this.closeModal}>
+              <span className="closebtn glyphicon glyphicon-remove"></span>
+            </Button>
+          </Link><br/>
+          <fieldset>
+            <label for="address"">Address:</label>
+            <input type="text" id="address" name="address" value={this.state.address} onChange={this.handleTextChange}></input>
+            
+            <label for="city">City:</label>
+            <select id="city" name="city" value={this.state.city} onChange={this.handleSelectChange}>
+              <option value="database cities go here" id="database cities go here">Database Cities Go Here</option>
+            </select>
+        
+            <label for="state">State:</label>
+            <select id="state" name="state" value={this.state.state} onChange={this.handleSelectChange}>
+              <option value="database states go here" id="database states go here">Database States Go Here</option>
+            </select>
+        
+            <label for="zipcode"">Zip Code:</label>
+            <input type="text" id="zipcode" name="zipcode" value={this.state.zipcode} onChange={this.handleTextChange}></input>
+          </fieldset>
+          <div className='button-center'>
+            <br/>
+            <Button bsStyle="success" bsSize="small" onClick={this.onClick}>Add New Destination</Button>
+          </div>
+          </Modal>
+        </div>
+      )
+    } else {
+        return (
+          <div>
+            <Button bsStyle="success" bsSize="small" onClick={this.openModal>
+              <span className="glyphicon glyphicon-plus"></span>
+            </Button>
+            <Modal isOpen={this.state.modalIsOpen}
+              onAfterOpen={this.afterOpenModal}
+              onRequestClose={this.closeModal}
+              contentLabel="Add Destination"
+              className="Modal">
+            <div className='button-center'>
+              <h3>{this.state.messageFromServer}</h3>
+              <Link to={{pathname: '/', search: ''}} style={{textDecoration: 'none'}}>
+                <Button bsStyle="success" bsSize="mini" onClick={this.closeModal}>Close the Dialog</Button>
+              </Link>
+            </div>
+            </Modal>
+          </div>
+        )
+    }
+  }
+}
+
+export default Add;
